e6b70d84a1291e95cf7a2f039bb952fc530b9cb1
MusiurAlam <musiur.opu@gmail.com>
2022-08-15 05:06:04 +0600

    dashboard issue fixed

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,7 +6,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import CommonProfile from "../components/usual/CommonProfile";
 import DonationTable from "../components/usual/DonationTable";
 import Settings from "../components/usual/Settings";
@@ -30,45 +30,45 @@ const sideNavList = [
     icon: faGear,
   },
 ];
+const doc = typeof document !== "undefined";
 
 const Dashboard = () => {
   const [currentTab, setCurrentTab] = useState(0);
   const [user, setUser] = useContext(UserContext);
-  const router = useRouter();
-  useEffect(() => {
-    !user.login && router.push("/");
-  }, [user]);
 
-  if(user.login){
+  const router = useRouter();
+
+  if (user.login) {
     return (
-    <div className="dashboard__container">
-      <div className="dc__sidenav">
-        <ul>
-          {sideNavList.map((item, i) => {
-            const { link, icon } = item;
-            return (
-              <li
-                key={i}
-                onClick={() => setCurrentTab(i)}
-                className={`${currentTab === i && "dc__sidenav__li_active"}`}
-              >
-                <FontAwesomeIcon icon={icon} style={{ maxWidth: "30px" }} />
-                <span>{link}</span>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="dashboard__container">
+        <div className="dc__sidenav">
+          <ul>
+            {sideNavList.map((item, i) => {
+              const { link, icon } = item;
+              return (
+                <li
+                  key={i}
+                  onClick={() => setCurrentTab(i)}
+                  className={`${currentTab === i && "dc__sidenav__li_active"}`}
+                >
+                  <FontAwesomeIcon icon={icon} style={{ maxWidth: "30px" }} />
+                  <span>{link}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="dc__field">
+          {currentTab === 0 && <CommonProfile />}
+          {currentTab === 1 && <UserList />}
+          {currentTab === 2 && <DonationTable />}
+          {currentTab === 3 && <Settings />}
+        </div>
       </div>
-      <div className="dc__field">
-        {currentTab === 0 && <CommonProfile />}
-        {currentTab === 1 && <UserList />}
-        {currentTab === 2 && <DonationTable />}
-        {currentTab === 3 && <Settings />}
-      </div>
-    </div>
-  );
-  }else{
-    return <div></div>
+    );
+  } else {
+    doc && !user.login && router.push("/");
+    return <div></div>;
   }
 };
 
