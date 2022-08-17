620334c393d0a3812822bb830e30f67e84981235
MusiurAlam <musiur.opu@gmail.com>
2022-08-17 06:01:46 +0600

    al events fetch done

--- components/dashboard/CreateEvent.js
+++ components/dashboard/CreateEvent.js
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import FormStyles from "../../styles/modules/form.module.scss";
 
 const InitialFormData = {
   title: "",
@@ -14,6 +16,7 @@ const CreateEvent = () => {
 
   const handleOnChange = (e) => {
     const { name, value } = e.target;
+
     setFormData({ ...formData, [name]: value });
   };
 
@@ -22,9 +25,18 @@ const CreateEvent = () => {
     setErrorMessage(validator(formData));
   };
 
+  const fetchCreatEvent = async () => {
+    const response = await axios.post(
+      `http://localhost:9000/events/create`,
+      formData
+    );
+    console.log(response);
+  };
+
   useEffect(() => {
     if (Object.keys(errorMessage).length === 0) {
       // call api
+      fetchCreatEvent();
     } else {
       console.log(errorMessage);
     }
@@ -32,9 +44,88 @@ const CreateEvent = () => {
 
   return (
     <div>
-      <h3>Create New Event</h3>
-      <form>
-        <button onClick={handleSubmit}>Create</button>
+      <h2>Create a new event to collect donation</h2>
+
+      <form className={FormStyles.formBox}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <br />
+          <input
+            onChange={handleOnChange}
+            name="title"
+            type="text"
+            id="title"
+            className={FormStyles.inputBox}
+          />
+          {errorMessage.title && (
+            <div className="errorStyle">{errorMessage.title}</div>
+          )}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="description">Description</label>
+          <br />
+          <textarea
+            onChange={handleOnChange}
+            name="description"
+            type="text"
+            id="description"
+            className={FormStyles.inputBox}
+            rows="7"
+          />
+          {errorMessage.description && (
+            <div className="errorStyle">{errorMessage.description}</div>
+          )}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="deadline">Deadline</label>
+          <br />
+          <input
+            onChange={handleOnChange}
+            name="deadline"
+            type="date"
+            id="deadline"
+            className={FormStyles.inputBox}
+          />
+          {errorMessage.deadline && (
+            <div className="errorStyle">{errorMessage.deadline}</div>
+          )}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="targeted_amount">Targeted Amount</label>
+          <br />
+          <input
+            onChange={handleOnChange}
+            name="targeted_amount"
+            type="number"
+            id="targeted_amount"
+            className={FormStyles.inputBox}
+          />
+          {errorMessage.targeted_amount && (
+            <div className="errorStyle">{errorMessage.targeted_amount}</div>
+          )}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="raised_amount">Raised Amount</label>
+          <br />
+          <input
+            onChange={handleOnChange}
+            name="raised_amount"
+            type="number"
+            id="raised_amount"
+            className={FormStyles.inputBox}
+          />
+          {errorMessage.raised_amount && (
+            <div className="errorStyle">{errorMessage.raised_amount}</div>
+          )}
+        </div>
+        <br />
+        <button onClick={handleOnSubmit} className="btn-primary">
+          Create
+        </button>
       </form>
     </div>
   );
@@ -44,5 +135,20 @@ export default CreateEvent;
 
 const validator = (data) => {
   let err = {};
+  if (!data.title.trim()) {
+    err.title = "title is required!";
+  }
+  if (!data.description.trim()) {
+    err.description = "description is required!";
+  }
+  if (!data.deadline.trim()) {
+    err.deadline = "deadline is required!";
+  }
+  if (!data.targeted_amount.trim()) {
+    err.targeted_amount = "targeted_amount is required!";
+  }
+  if (!data.raised_amount.trim()) {
+    err.raised_amount = "raised_amount is required!";
+  }
   return err;
 };
--- components/dashboard/Events.js
+++ components/dashboard/Events.js
@@ -1,5 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Events = () => {
-  return <div>Events</div>;
+  const [events, setEvents] = useState([]);
+
+  const fetchEventsData = async () => {
+    const response = await axios.get(`http://localhost:9000/events`);
+    if (response.status === 200) {
+      setEvents(response.data.result);
+    }
+  };
+  useEffect(() => {
+    fetchEventsData();
+  }, []);
+  console.log(events);
+  return (
+    <div>
+      <h2>All events for collecting donation</h2>
+      <div>
+        {events.map((event, i) => {
+          return <EventCard key={i} data={event} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Events;
+
+const EventCard = ({ data }) => {
+  console.log(data);
+  const { title, description, targeted_amount, raised_amount, deadline } = data;
+  return (
+    <div>
+      <div></div>
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <p>{targeted_amount}</p>
+        <p>{raised_amount}</p>
+        <p>{deadline}</p>
+        <div>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
